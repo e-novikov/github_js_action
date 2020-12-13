0eedcd2ee1d2805a425d3167e02964fa41c13f4d
Novikov Egor <e.novikov@socialquantum.com>
2020-12-14 01:06:22 +0300

    add commit compare

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { GitHub } = require('@actions/github/lib/utils');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,64 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  const client = new GitHub(core.getInput('token',{required = true}));
+
+  const eventName = context.eventName;
+
+  base = ""
+  head = ""
+
+  switch (eventName) {
+    case 'pull_request':
+      base = context.payload.pull_request?.base?.sha;
+      head = context.payload.pull_request?.head?.sha;
+      break
+    case 'push':
+      base = context.payload.before;
+      head = context.payload.after;
+      break
+    default:
+      core.setFailed(
+        `This action only supports pull requests and pushes, ${context.eventName} events are not supported. ` +
+          "Please submit an issue on this action's GitHub repo if you believe this in correct."
+      );
+  }
+
+  core.info(`Base commit: ${base}`);
+  core.info(`Head commit: ${head}`);
+
+  if (!base || !head) {
+    core.setFailed(
+      `The base and head commits are missing from the payload for this ${context.eventName} event. ` +
+        "Please submit an issue on this action's GitHub repo."
+    )
+
+    // To satisfy TypeScript, even though this is unreachable.
+    base = ''
+    head = ''
+  }
+
+  // Use GitHub's compare two commits API.
+  // https://developer.github.com/v3/repos/commits/#compare-two-commits
+  const response = await client.repos.compareCommits({
+    base,
+    head,
+    owner: context.repo.owner,
+    repo: context.repo.repo
+  })
+
+  // Ensure that the request was successful.
+  if (response.status !== 200) {
+    core.setFailed(
+      `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. ` +
+        "Please submit an issue on this action's GitHub repo."
+    )
+  }
+
+  console.log(`Response: ${response.data}`);
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
